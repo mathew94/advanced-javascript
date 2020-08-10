3f8f7fc701314bdd5578ba8f583fe19a72a439fd
mathew94 <mathewrodrick94@hotmail.com>
2020-08-10 13:20:34 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// fasly values ////// condition false hobe
+// 0
+// false
+// ""
+// undefined
+// null
+// NaN
+
+// truthy values //// condition
+// '0', " ", []
+
+const name = "Mathew";
+
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
